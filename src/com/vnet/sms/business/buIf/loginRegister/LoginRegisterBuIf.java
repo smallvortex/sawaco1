67280fbe58b4498ac0bb66fbee14ac0e546e7c0a
smallvortex <haianbaggio@yahoo.com>
2017-05-10 15:38:38 +0700

    commet
    add commetn finduser

--- src/com/vnet/sms/business/buIf/loginRegister/LoginRegisterBuIf.java
+++ src/com/vnet/sms/business/buIf/loginRegister/LoginRegisterBuIf.java
@@ -3,7 +3,7 @@ package com.vnet.sms.business.buIf.loginRegister;
 import com.vnet.sms.dao.model.Users;
 
 public interface LoginRegisterBuIf {
-
+//findUser
 	public Users findUser(String userName, String password);
 	public String getInfo() ;
 }
